b768a8cd1d7566f63bc6aa679d39c5de37bd61b0
Jtwulf <jtwulf1005@icloud.com>
2023-10-22 01:15:48 +0900

    fix git secret issue

--- git_push_journal.sh
+++ git_push_journal.sh
@@ -6,16 +6,14 @@ day=$(date +%d)
 date_string="$year-$month-$day"
 journal_file=~/Documents/justin/personal/journal/$year/$month/$date_string.txt
 
-echo day
-echo day
-echo day
+cd "$HOME/Documents/justin/personal/journal"
 
-git secret add "$journal_file"
+git secret add "$year/$month/$date_string.txt"
 git secret hide
 
-cd "$HOME/Documents/justin/personal/journal"
-git add "$journal_file.secret"
-git commit -m "Journal entry for $date_string"
+git add "$year/$month/$date_string.txt.secret"
+# git commit -m "Journal entry for $date_string"
+git commit -m "Journal entry for test1"
 git push origin main
 
 echo "Journal entry for $date_string is committed and pushed to main branch."
