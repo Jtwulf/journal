5a13051ea571dd35439a0b44862dcb9a2349868d
Jtwulf <jtwulf1005@icloud.com>
2023-11-23 21:53:50 +0900

    change format of jornal file

--- journal_entry.sh
+++ journal_entry.sh
@@ -16,17 +16,19 @@ header+="/*   $date_string_long                         */\n"
 header+="/*                                                     */\n"
 header+="/*   By: $user                                    */\n"
 header+="/*                                                     */\n"
-header+="/* *************************************************** */\n"
+header+="/* *************************************************** */\n\n"
+header+="# Today's Schedule\n\n\n\n# Today's Goal\n\n\n\n# Journal"
 
 file_path=~/Documents/justin/personal/journal/src/$year/$month/$date_string_short.txt
 
 if [ -e "$file_path" ]; then
     echo "File $file_path already exists."
-    vim "$file_path" +9
+    vim "$file_path" +11
 else
     mkdir -p $(dirname "$file_path")
     touch "$file_path"
     echo "$header" >> "$file_path"
     echo "\n" >> "$file_path"
-    vim "$file_path" +9
+    vim "$file_path" +11
 fi
+
